114070c01b771ad4c623ad0ab5511a55a645af35
kbtsaharat <saharatnew596@gmail.com>
2023-09-03 14:02:02 +0900

    Frist push

--- resources/views/whatis.blade.php
+++ resources/views/whatis.blade.php
@@ -858,7 +858,7 @@
                                 </svg>
                             </div> -->
 
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Tips</h2>
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">What is </h2>
 
                             <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                 "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
@@ -867,7 +867,7 @@
                     </div>
                 </div>
             </div>
-            
+
         </div>
         
     </div>
