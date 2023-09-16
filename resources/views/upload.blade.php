38e16c7bbc378dd6cdc32c316fe09b4087e55699
kbtsaharat <saharatnew596@gmail.com>
2023-09-16 15:09:38 +0700

    ver.0.5.2

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -5,37 +5,38 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+    <div class="py-12 ">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
 
-                    <div class="mb-4">
-                        <!-- Input for file selection -->
-                        <label for="file_path" class="block mb-2 text-gray-700">Choose a file:</label>
-                        <div class="relative border border-gray-400 rounded-md p-2 bg-white shadow-md">
-                            <input id="file_path" name="file_path" type="file" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer" accept="image/*" capture="camera" onchange="displayImage(this)" />
-                            <div class="text-center">
-                                <svg class="mx-auto h-6 w-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                                </svg>
-                                <p class="mt-1 text-sm text-gray-600">Select a file</p>
-                            </div>
+                <div class="mb-4">
+                    <!-- Input for file selection -->
+                    <label for="file_path" class="block mb-2 text-gray-700 dark:text-gray-300">Choose a file:</label>
+                    <div class="relative border border-gray-400 rounded-md p-2 bg-white dark:bg-gray-700 shadow-md">
+                        <input id="file_path" name="file_path" type="file" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer" accept="image/*" capture="camera" onchange="displayImage(this)" />
+                        <div class="text-center">
+                            <svg class="mx-auto h-6 w-6 text-gray-400 dark:text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+                            </svg>
+                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-300">Select a file</p>
                         </div>
                     </div>
-
-                    <div class="flex justify-center items-center bg-white border border-gray-300 shadow-lg rounded-lg p-4 text-center">
-                        <!-- Display the selected image -->
-                        <img id="selectedImage" src="{{ asset('images/placeholder.jpg') }}" alt="Selected File" class="rounded-lg border-4 border-blue-500" style="max-width: 100%; max-height: 400px; object-fit: cover;">
-                    </div>
-
-                    <!-- Display the detected objects -->
-                    <div id="show"></div>
-
                 </div>
+
+                <div class="flex justify-center items-center bg-white dark:bg-gray-700 border border-gray-300 shadow-lg rounded-lg p-4 text-center">
+                    <!-- Display the selected image -->
+                    <img id="selectedImage" src="{{ asset('images/placeholder.jpg') }}" alt="Selected File" class="rounded-lg border-4 border-blue-500" style="max-width: 100%; max-height: 400px; object-fit: cover;">
+                </div>
+
+                <!-- Display the detected objects -->
+                <div id="show"></div>
+
             </div>
         </div>
     </div>
+</div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
